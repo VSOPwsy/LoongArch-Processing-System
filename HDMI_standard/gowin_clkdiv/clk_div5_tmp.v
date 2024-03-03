b40937b5adcb36ca12643117793d16da50eecc55
FY <138192626+FYPIEAK@users.noreply.github.com>
2024-03-03 17:12:37 +0800

    Add files via upload

---
+++ HDMI_standard/gowin_clkdiv/clk_div5_tmp.v
@@ -0,0 +1,19 @@
+//Copyright (C)2014-2024 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.9.01 (64-bit)
+//Part Number: GW2A-LV18PG256C8/I7
+//Device: GW2A-18
+//Device Version: C
+//Created Time: Thu Feb 29 20:02:50 2024
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    clk_ your_instance_name(
+        .clkout(clkout_o), //output clkout
+        .hclkin(hclkin_i), //input hclkin
+        .resetn(resetn_i) //input resetn
+    );
+
+//--------Copy end-------------------
