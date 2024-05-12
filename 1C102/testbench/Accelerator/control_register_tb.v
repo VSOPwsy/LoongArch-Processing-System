c7f0fe0eefd7564d2bee387b0c36aa3477c28c21
FY <12111342@mail.sustech.edu.cn>
2024-05-12 15:22:24 +0800

    sdfghj

--- 1C102/testbench/Accelerator/control_register_tb.v
+++ 1C102/testbench/Accelerator/control_register_tb.v
@@ -11,11 +11,14 @@ module control_register_tb;
   reg  [31:0] n;
   reg  start;
   wire dma_start;
+  reg dma_valid;
+  reg dma_ready;
   reg  dma_done;
   reg  [255:0] dma_data;
   wire  [255:0] buf_data_in;
   wire  read_a;
   wire  read_b;
+  integer i;
 
   control_register # (
     .SIZE(SIZE)
@@ -32,6 +35,8 @@ module control_register_tb;
     .b_in_mode(1),
     .start(start),
     .dma_start(dma_start),
+    .dma_valid(dma_valid),
+    .dma_ready(dma_ready),
     .dma_done(dma_done),
     .dma_data(dma_data),
     .buf_data_in(buf_data_in),
@@ -63,13 +68,31 @@ module control_register_tb;
       @(posedge clk);
       start = 1;
       dma_data = 256'h1234_5678;
-      wait(dma_start);
-      #20;
-      dma_done = 1;
+
 
       #10000000;
       $finish;
   end
   
+  forever begin
+    wait(dma_start);
+    #20;
+    @(posedge clk);
+
+    for ( i = 0; i < SIZE/8 ; i = i+1) begin
+      @(posedge clk);
+      dma_ready = 1;
+      dma_valid = 1;
+      if (i == SIZE/8 - 1) begin
+        dma_done = 1;
+      end
+      @(posedge clk);
+      dma_ready = 0;
+      dma_valid = 0;
+      dma_done = 0;
+    end
+
+  end
+
 
 endmodule
